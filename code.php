485d6f1cbbe37b3e13f2e4611e0906da290ce035
Aashab Tajwar <tajwaraashab@gmail.com>
2021-11-22 19:52:02 +0600

    added the last change to the script

--- code.php
+++ code.php
@@ -1,7 +1,7 @@
 <?php
 
 $build_container = shell_exec("sudo docker build -t 'clang-doc' .");
-$run_container = shell_exec("sudo docker run -ti --memory='6m' --oom-kill-disable --cpus='1.0' 'clang-doc'");
+$run_container = shell_exec("sudo docker run -i --memory='6m' --oom-kill-disable --cpus='1.0' 'clang-doc' < input.txt");
 
 echo $run_container;
 
